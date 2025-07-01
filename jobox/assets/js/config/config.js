c98745db58c215ab2128b1bd85381de4427a6075
notbetha <bethapunto@gmail.com>
2025-07-01 14:18:46 -0400

    visual changes

--- jobox/assets/js/config/config.js
+++ jobox/assets/js/config/config.js
@@ -3,7 +3,7 @@ const isDev =
   window.location.hostname === 'localhost';
 
   window.BASE_URL_API = (window.location.hostname.includes('localhost') || window.location.hostname === '127.0.0.1')
-  ? 'http://172.25.100.201:3000/v1'
+  ? 'http://localhost:3000'
   : 'https://tuempleo.cl/api';
 
 console.log('✅ config.js cargado, BASE_URL_API =', window.BASE_URL_API);
