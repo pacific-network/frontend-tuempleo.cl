2381ab425e606e96b624dc91699b5cccebee6dd5
Paulo Andrés Ramirez Ciani <90012935+Ramirezciani@users.noreply.github.com>
2025-12-05 12:28:18 -0300

    fix rutas (#129)

--- jobox/assets/js/modules/postulaciones/core/proceso_seleccion/contratados.js
+++ jobox/assets/js/modules/postulaciones/core/proceso_seleccion/contratados.js
@@ -80,7 +80,7 @@ export function attachVerCvEvents() {
       console.log("➡️ Redirigiendo con data:", encoded);
 
       // Redirección limpia
-      window.location.href = `empresas/employer-view-candidate.html?data=${encoded}`;
+      window.location.href = `/empresas/employer-view-candidate.html?data=${encoded}`;
     });
   });
 }
