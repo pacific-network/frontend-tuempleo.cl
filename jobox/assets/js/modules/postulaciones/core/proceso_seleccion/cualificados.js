31c7d3176f55a6a74069117afa7b5f9976335a19
Ramirezciani <pa.ramirezciani13@gmail.com>
2025-12-05 12:27:34 -0300

    fix rutas

--- jobox/assets/js/modules/postulaciones/core/proceso_seleccion/cualificados.js
+++ jobox/assets/js/modules/postulaciones/core/proceso_seleccion/cualificados.js
@@ -85,7 +85,7 @@ export function attachVerCvEvents() {
       console.log("➡️ Redirigiendo con data:", encoded);
 
       // Redirección limpia
-      window.location.href = `empresas/employer-view-candidate.html?data=${encoded}`;
+      window.location.href = `/empresas/employer-view-candidate.html?data=${encoded}`;
     });
   });
 }
